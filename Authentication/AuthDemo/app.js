1f5c10a5f87a46644d8661157d9ae8208f423ac6
Lang Kenney <Langk411@yahoo.com>
2016-06-14 01:27:51 +0000

    added authentication

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -6,11 +6,11 @@ var express = require("express"),
      User = require("./models/user"),
      passportLocalMongoose = require("passport-local-mongoose");
      
-mongoose.connect("mongodb://localhost/auth_demp_app");
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine","ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "This is a secret",
     resave: false,
@@ -20,16 +20,71 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//===============================================
+//====Routes=======
+//===============================================
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret",function(req, res){
-    res.render("secret");
+app.get("/register", function(req, res) {
+    res.render("register")
+});
+
+//handels user sign up
+app.post("/register", function(req, res){
+    //req.body.username
+    //req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if (err){
+           console.log(err);
+           return res.render("register");
+       } else {
+           //passport.authenticate("twitter")(req, res, function(){
+           //passport.authenticate("facebook")(req, res, function(){
+           passport.authenticate("local")(req, res, function(){
+               res.render("secret");
+           });
+       }
+    });
+});
+
+//login routes
+//renders form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//login logic
+//middlewear
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}),function(req, res){
+});
+
+//logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/")
 })
+
+
+app.get("/secret", isLoggedIn,function(req, res){
+    res.render("secret");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login")
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running...");
